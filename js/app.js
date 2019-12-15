30e7b1aa2e7ce7bf2cb2a0b55a427220ebca74e0
tepoknyamuk <56757112+tepoknyamuk@users.noreply.github.com>
2019-12-15 18:36:16 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-// variable test
+// Animate Smooth Scroll
+$('#gallery').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
